eb725fe38036ce0312666d2e40ee44f171aefcdd
Vuong7720 <vuong7720@gmail.com>
2025-10-16 00:23:13 +0700

    fix messager

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wedding-invite';
+  isAppReady: boolean = false; 
+enterWebsite(): void {
+    this.isAppReady = true; 
+    
+    // GỌI HÀM PHÁT NHẠC
+    // Vì người dùng đã tương tác (click), trình duyệt sẽ cho phép phát nhạc.
+    const audioEl = document.getElementById('backgroundMusic') as HTMLAudioElement;
+    if (audioEl) {
+        audioEl.play().catch(e => console.log("Không thể phát nhạc ngay lập tức. Cung cấp nút điều khiển."));
+    }
+  }
 }
--- src/app/music-player.component.ts
+++ src/app/music-player.component.ts
@@ -47,4 +47,6 @@ export class MusicPlayerComponent implements AfterViewInit {
       });
     }
   }
+
+  
 }
