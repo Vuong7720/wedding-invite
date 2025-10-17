7b5a952961613828d4d9d314e2b625127149baf8
Vuong7720 <vuong7720@gmail.com>
2025-10-17 13:03:22 +0700

    update_final1

--- src/app/components/albums/albums.component.ts
+++ src/app/components/albums/albums.component.ts
@@ -10,12 +10,14 @@ export class AlbumsComponent implements OnInit {
   // Mảng chứa đường dẫn ảnh cưới của bạn
   // THAY THẾ 'URL_ANH_...' bằng đường dẫn URL thực tế
   photos = [
-    { url: 'assets/images/codau.jpg', description: 'Khoảnh khắc lãng mạn' },
-    { url: 'assets/images/chure.jpg', description: 'Ánh mắt trao nhau' },
-    { url: 'assets/images/w1.jpg', description: 'Nắm tay đi hết cuộc đời' },
-    { url: 'assets/images/w3.jpg', description: 'Hạnh phúc giản đơn' },
-    { url: 'assets/images/w4.jpg', description: 'Nụ cười rạng rỡ' },
-    { url: 'assets/images/wed-1.jpg', description: 'Tình yêu vĩnh cửu' },
+    { url: 'assets/image_weding/wd1.jpg', description: 'Khoảnh khắc lãng mạn' },
+    { url: 'assets/image_weding/wd2.jpg', description: 'Ánh mắt trao nhau' },
+    { url: 'assets/image_weding/wd3.jpg', description: 'Nắm tay đi hết cuộc đời' },
+    { url: 'assets/image_weding/wd4.jpg', description: 'Hạnh phúc giản đơn' },
+    { url: 'assets/image_weding/wd5.jpg', description: 'Nụ cười rạng rỡ' },
+    { url: 'assets/image_weding/wd6.jpg', description: 'Tình yêu vĩnh cửu' },
+    { url: 'assets/image_weding/wd7.jpg', description: 'Tình yêu vĩnh cửu' },
+    { url: 'assets/image_weding/wd8.jpg', description: 'Tình yêu vĩnh cửu' },
     // Thêm bao nhiêu ảnh tùy thích
   ];
 
